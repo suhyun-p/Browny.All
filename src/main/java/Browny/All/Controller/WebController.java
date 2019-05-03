f224fef83145d66f64e8aad63b23aee6213e5fc2
박수현 <suhpark@ebay.com>
2019-05-03 14:18:05 +0900

    load classList on index

--- src/main/java/Browny/All/Controller/WebController.java
+++ src/main/java/Browny/All/Controller/WebController.java
@@ -1,14 +1,21 @@
 package Browny.All.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping(value = "/")
 public class WebController {
     @RequestMapping(value = "/")
     public String Index(Model model) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object> ret = restTemplate.getForEntity("http://localhost:8080/api/class/getClassList", Object.class);
+        model.addAttribute("classList", ret.getBody());
+
         return "/index";
     }
 }
