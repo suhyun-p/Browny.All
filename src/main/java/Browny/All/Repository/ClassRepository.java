cad5e19cc964c293021f86238285480f30c2a544
박수현 <suhpark@ebay.com>
2019-06-10 18:05:45 +0900

    Sorting

--- src/main/java/Browny/All/Repository/ClassRepository.java
+++ src/main/java/Browny/All/Repository/ClassRepository.java
@@ -3,5 +3,11 @@ package Browny.All.Repository;
 import Browny.All.Entity.ClassT;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 public interface ClassRepository extends JpaRepository<ClassT, Long> {
+    List<ClassT>  findAllByOrderByStartDateDesc();
+    List<ClassT>  findAllByEndDateIsGreaterThanEqualOrderByStartDateDesc(LocalDate today);
 }
--- src/main/java/Browny/All/Service/ClassService.java
+++ src/main/java/Browny/All/Service/ClassService.java
@@ -41,7 +41,8 @@ public class ClassService {
     @Transactional
     public List<ClassSimpleT> getClassSimpleList() {
         List<ClassSimpleT> classSimpleList = new ArrayList<>();
-        List<ClassT> classTList = classRepository.findAll();
+        List<ClassT> classTList = classRepository.findAllByOrderByStartDateDesc();
+        // List<ClassT> classTList = classRepository.findAllByEndDateIsGreaterThanEqualOrderByStartDateDesc(LocalDate.now());
         for(ClassT classT : classTList)
             classSimpleList.add(new ClassSimpleT(classT));
 
