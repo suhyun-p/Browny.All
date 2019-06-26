2788a4877ccbc9afcdde8cfbba1a90ec74777eba
박수현 <suhpark@ebay.com>
2019-06-26 14:58:12 +0900

    regUser form

--- src/main/resources/assets/js/admin/regUser.js
+++ src/main/resources/assets/js/admin/regUser.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
+    $("#instructor").click(function() {
+        if($("#instructor").prop("checked")) {
+            $("#instructorInfo").show();
+        }
+        else {
+            $("#instructorInfo").hide();
+        }
+    });
 
+    $("#careerAdd").click(function() {
+
+    });
+
+    $("#careerDel").click(function() {
+
+    });
 });
