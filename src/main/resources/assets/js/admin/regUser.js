23d526b1b8d5ca443a0375135abda1ae661b3ea7
박수현 <suhpark@ebay.com>
2019-06-26 17:25:58 +0900

    regUser form

--- src/main/resources/assets/js/admin/regUser.js
+++ src/main/resources/assets/js/admin/regUser.js
@@ -7,12 +7,14 @@ $(document).ready(function () {
             $("#instructorInfo").hide();
         }
     });
-
-    $("#careerAdd").click(function() {
-
-    });
-
-    $("#careerDel").click(function() {
-
-    });
 });
+
+function addCareer() {
+    var html = "<div class=\"col-md-12 mb-3\">";
+    html += "<div class=\"custom-control-inline\" style=\"width: 100%\">";
+    html += "<input type=\"text\" class=\"form-control\" name=\"career\" placeholder=\"\">&nbsp;";
+    html += "<button type=\"button\" class=\"btn btn-primary btn-sm\" onclick=\"javascript:$(this).parent().parent().remove();\">Del</button>";
+    html += "</div>";
+    html += "</div>";
+    $("#divCareer").append(html)
+}
