058ec397f15c927cc842c77500c562d2c03330dd
박수현 <suhpark@ebay.com>
2019-06-27 12:44:45 +0900

    강사정보

--- src/main/resources/assets/js/admin/regUser.js
+++ src/main/resources/assets/js/admin/regUser.js
@@ -14,6 +14,58 @@ $(document).ready(function () {
         data["sex"] = $(':input[name=sex]:radio:checked').val();
         data["instructor"] = $("#instructor").is(":checked");
 
+        if($("#instructor").is(":checked")) {
+            data["account"] = $("#account").val();
+
+            var contactList = new Array();
+            if($("#phoneNo").val() != "") {
+                var phoneNo = {"type":"P", "contact":$("#phoneNo").val()}
+                contactList.push(phoneNo);
+            }
+
+            if($("#kakaoTalk").val() != "") {
+                var kakaoTalk = {"type":"K", "contact":$("#kakaoTalk").val()}
+                contactList.push(kakaoTalk);
+            }
+
+            if($("#facebook").val() != "") {
+                var facebook = {"type":"F", "contact":$("#facebook").val()}
+                contactList.push(facebook);
+            }
+
+            if($("#instagram").val() != "") {
+                var instagram = {"type":"I", "contact":$("#instagram").val()}
+                contactList.push(instagram);
+            }
+
+            if($("#youtubeUrl").val() != "") {
+                var youtube = {"type":"Y", "name":$("#youtubeName").val(), "contact":$("#youtubeUrl").val()}
+                contactList.push(youtube);
+            }
+
+            if($("#daumCafeUrl").val() != "") {
+                var daumCafe = {"type":"DC", "name":$("#daumCafeName").val(), "contact":$("#daumCafeUrl").val()}
+                contactList.push(daumCafe);
+            }
+
+            if($("#naverCafeUrl").val() != "") {
+                var naverCafe = {"type":"NC", "name":$("#naverCafeName").val(), "contact":$("#naverCafeUrl").val()}
+                contactList.push(naverCafe);
+            }
+
+            if($("#naverBandUrl").val() != "") {
+                var naverBand = {"type":"NB", "name":$("#naverBandName").val(), "contact":$("#naverBandUrl").val()}
+                contactList.push(naverBand);
+            }
+            data["contactList"] = contactList;
+
+            var careerList= new Array();
+            $("input[name='career']").each(function (index, career) {
+                careerList.push(career.value);
+            });
+            data["careerList"] = careerList;
+        }
+
         $.ajax({
             contentType: 'application/json',
             dataType: 'json',
